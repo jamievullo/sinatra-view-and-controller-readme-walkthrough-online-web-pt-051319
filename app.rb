7cf9bbea52b1b48c2d71a31b5ec7e08eee7280cd
Jamie Vullo <jvullo@protonmail.com>
2019-08-06 19:32:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
-    puts params
+    
     erb :reverse
   end
 
   post '/reverse' do
+    
     erb :reversed
   end
 
